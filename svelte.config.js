54871b132802598b2297c8ef3a9bd7b757df57e0
Patrick Tisdale <patrick.tisdale@gmail.com>
2021-07-30 17:41:25 -0500

    changed svelte.config.js again

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,11 @@
 /** @type {import('@sveltejs/kit').Config} */
 
+import adapter from '@sveltejs/adapter-netlify@next'
+
 
 const config = {
 	kit: {
+    adapter: adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
